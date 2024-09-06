c733ae568df75427a378f205aa1a69e6ba44e281
Ccottite <kuro_san@outlook.es>
2024-09-06 16:55:57 -0600

    Actualizacion responsive

--- src/components/Nabvar/Navbar.jsx
+++ src/components/Nabvar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import menu_icon from '../../assets/iconos/menu.png'
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
@@ -13,18 +14,24 @@ const Navbar = () => {
         })
     }, []);
 
+    const [mobileMenu, setMobileMenu] = useState(false)
+    const toggleMenu = () => {
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true)
+    }
+
     return (
         <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
             <img src={logo} alt="logo_lobo" className='logo' />
-            <ul>
+            <ul className={mobileMenu?'':'hide-mobile-menu'}>
                 <li><Link to='hero' smooth={true} offset={0} duration={500}>Inicio</Link></li>
-                <li><Link to='about' smooth={true} offset={-100} hashSpy={true} duration={500}>Sobre Nosotros</Link></li>
-                <li><Link to='servicios' smooth={true} offset={-275} duration={500}>Nuestros Productos</Link></li>
+                <li><Link to='about' smooth={true} offset={-100} hashSpy={true} duration={500}>Nosotros</Link></li>
+                <li><Link to='servicios' smooth={true} offset={-275} duration={500}>Productos</Link></li>
                 <li><Link to='maps' smooth={true} offset={-295} duration={500}>Ubicaciones</Link></li>
-                <li><Link to='items' smooth={true} offset={-320} duration={500}>Articulos Nuevos</Link></li>
-                <li><Link to='CardsGrid' smooth={true} offset={-380} duration={500}>Nuestras Promociones</Link></li>
+                <li><Link to='items' smooth={true} offset={-320} duration={500}>Articulos</Link></li>
+                <li><Link to='CardsGrid' smooth={true} offset={-380} duration={500}>Promociones</Link></li>
                 <li><Link to='contact' smooth={true} offset={0} duration={500} className='btn'>Contactanos</Link></li>
             </ul>
+            <img src={menu_icon} alt="menu_icon" className='menu-icon' onClick={toggleMenu} />
         </nav>
     )
 }
